acca62a83ae70e2bbb3dc43a0049473f93eb9148
prashB2D <prashanthgowdars555@gmail.com>
2026-03-11 09:43:54 +0530

    Initial project upload

---
+++ domitra/src/main/java/com/prashu23/domitra/DomitraApplication.java
@@ -0,0 +1,13 @@
+package com.prashu23.domitra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomitraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomitraApplication.class, args);
+	}
+
+}
